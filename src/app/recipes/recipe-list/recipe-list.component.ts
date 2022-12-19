564ef91af6c8b744e95d00e3e44f4a6587b92ed4
johnnysee <js.johnny.see@gmail.com>
2022-12-20 00:00:33 +0100

    displaying hardcoded recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://freesvg.org/img/RecipeBook.png'
+    ),
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://freesvg.org/img/RecipeBook.png'
+    ),
+  ];
   constructor() {}
 
   ngOnInit(): void {}
